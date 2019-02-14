0437c80cf6b203b7433d7dce4cbd6c4f9e5191e8
Florencia Lafuente <lafuente.florencia@gmail.com>
2019-02-13 22:04:47 -0300

    Add basic routing

--- server.js
+++ server.js
@@ -10,6 +10,22 @@ const init = async () => {
   console.log(`Server running at: ${server.info.uri}`)
 }
 
+server.route({
+  method: 'GET',
+  path: '/products',
+  handler:  (request, h) => {
+    return 'Hello products!'
+  }
+})
+
+server.route({
+  method: 'GET',
+  path: '/users',
+  handler:  (request, h) => {
+    return 'Hello users!'
+  }
+})
+
 process.on('unhandledRejection', (err) => {
   console.log(err)
   process.exit(1)
